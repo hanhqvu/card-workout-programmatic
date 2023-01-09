f03204e31cda753e51956a50f4fa6bac243e3174
hanhqvu <hanhqvu@users.noreply.github.com>
2023-01-09 20:15:53 +0900

    update: init programmatic ui cont

--- CardWorkout-Programmatic/CardSelectionVC.swift
+++ CardWorkout-Programmatic/CardSelectionVC.swift
@@ -9,21 +9,11 @@ import UIKit
 
 class CardSelectionVC: UIViewController {
 
+    let cardImageView = UIImageView()
+    let stopButton = CWButton(backgroundColor: .systemRed, title: "Stop!")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
